88b59e85e743ad10c04d83d534bd52826cf39dc1
Val Orekhov <vorekhov@withum.com>
2021-05-24 11:52:59 -0400

    JS models add'l tweaks for NPM publishing

--- ts/build.sh
+++ ts/build.sh
@@ -1,20 +1,28 @@
 
 #!/bin/sh
 BRANCH_NAME=${1:-master}
+PACKAGE_VERSION=${2:-0.0.0}
+SPEC_LOCATION=${3}
 
-echo $BRANCH_NAME
+echo "Building from branch/tag: $BRANCH_NAME"
+echo "Package version: $PACKAGE_VERSION"
 
 if [ ! -f ./openapi-generator-cli.jar ]; then
     wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.1.0/openapi-generator-cli-5.1.0.jar -O ./openapi-generator-cli.jar
 fi
 
-git clone --depth 1 --branch $BRANCH_NAME https://github.com/AICPA-ADS/AuditData-API.git _spec &> /dev/null
+if [ -r $SPEC_LOCATION ]; then
+    git clone --depth 1 --branch $BRANCH_NAME https://github.com/AICPA-ADS/AuditData-API.git _spec &> /dev/null
+    SPEC_LOCATION="_spec/AuditDataStandard.yml"
+fi
 
 mkdir -p build/ts
-cp ts/models/generator-tempalte/.openapi-generator-ignore build/ts
+cp ts/models/generator-template/.openapi-generator-ignore build/ts
 
-java -jar ./openapi-generator-cli.jar generate -i _spec/AuditDataStandard.yml -g typescript -t ts/models/generator-tempalte \
-    -c ts/config.yaml -o build/ts
+echo "Building from spec: $SPEC_LOCATION"
+
+java -jar ./openapi-generator-cli.jar generate -i $SPEC_LOCATION -g typescript -t ts/models/generator-template \
+    -c ts/config.yaml -o build/ts --additional-properties=npmVersion=$PACKAGE_VERSION
 
 rm -rf dist/js/models
 mkdir -p dist/js/models
